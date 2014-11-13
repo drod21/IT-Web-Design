54f6200fa7b307b2e7283d027fd1de9dfdeb2a4c
Derek <drod2169@gmail.com>
2014-11-13 07:54:43 -0500

    More fixes to the project

--- public_html/project/questionnaire2.php
+++ public_html/project/questionnaire2.php
@@ -1,5 +1,4 @@
-<html>
-<body>
+<html><body>
 <title>View Questionnaire Records</title>
 <HR SIZE="3" WIDTH="75%" COLOR="darkgreen">
 <center><font size=6 color=black>View Questionnaire Records</font></center>
@@ -7,7 +6,7 @@
 <?php
 $db = mysql_connect("localhost", "derek23", "6tfc%RDX") or die(mysql_error());
 mysql_select_db("derek23",$db) or die(mysql_error());
-$query = "SELECT iname, AVG(objective), AVG(interest) FROM questionnaire GROUP BY iname";
+$query = "SELECT iname, AVG(objective), AVG(interest), AVG(overall) FROM questionnaire GROUP BY iname";
 $result = mysql_query($query) or die(mysql_error());
 $num_rows=mysql_num_rows($result);?>
 <form action="questionnaire2.php" method="post">
@@ -15,31 +14,10 @@ $num_rows=mysql_num_rows($result);?>
 <p>Select an Instructor to see the average scores : <select name="instructor" SIZE=<?php echo $num_rows?>>
 <?php while($row = mysql_fetch_array($result)){?>
 <option> <?php echo $row['iname']; }?>
-</select>
-</p>
-<?php
-$query = "SELECT ctitle, AVG(objective), AVG(interest) FROM questionnaire GROUP BY ctitle";
-$result = mysql_query($query) or die(mysql_error());
-$num_rows=mysql_num_rows($result);?>
-<br>
-<p>Select a course to see the average scores : <select name="ctitle" SIZE=<?php echo $num_rows?>>
-<?php while($row = mysql_fetch_array($result)){?>
-<option> <?php echo $row['ctitle']; }?>
-</select>
-</p>
-<?php
-$query = "SELECT semester, AVG(objective), AVG(interest) FROM questionnaire GROUP BY semester";
-$result = mysql_query($query) or die(mysql_error());
-$num_rows=mysql_num_rows($result);?>
-<br>
-<p>Select a semester to see the average scores : <select name="semester" SIZE=<?php echo $num_rows?>>
-<?php while($row = mysql_fetch_array($result)){?>
-<option> <?php echo $row['semester']; }?>
-</select>
-</p>
+</select></p>
+
 <p>
-<input type="submit"></input>
-</p>
+<input type="submit"></input></p>
 </form>
 <?php
 $instructor = $_REQUEST['instructor'];
@@ -50,7 +28,8 @@ echo "The average score of the Course objectives for ". $row['iname']. " is ".$r
 echo "<br />";
 echo "The average score of the Course interest for ". $row['iname']. " is ".$row['AVG(interest)'];
 echo "<br />";
- 
+echo "The overall average score of the Course for ". $row['iname']. " is ".$row['AVG(overall)'];
+echo "<br />"; 
 }
 }
 ?>
